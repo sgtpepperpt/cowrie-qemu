71a731f110cea283be458c684bd5342803f14ba6
Guilherme Borges <guilhermerosasborges@gmail.com>
2019-06-15 03:41:59 +0100

    network handler assigns ips for every host

--- network_handler.py
+++ network_handler.py
@@ -6,13 +6,22 @@ import util
 
 def create_network(connection):
     network_xml = util.read_file('config_files/default_network.xml')
+
+    sample_host = '<host mac=\'{mac_address}\' name=\'{name}\' ip=\'{ip_address}\'/>\n'
+    hosts = ''
+
+    for i in range(2, 255):
+        mac_address = 'aa:bb:cc:dd:ee:' + hex(i)[2:]
+        vm_name = 'vm' + str(i)
+        ip_address = '192.168.150.' + str(i)
+        hosts += sample_host.format(mac_address=mac_address, name=vm_name, ip_address=ip_address)
+
     network_config = network_xml.format(network_name='cowrie',
                                         iface_name='virbr2',
                                         default_gateway='192.168.150.1',
-                                        dhcp_range_start='192.168.150.127',
+                                        dhcp_range_start='192.168.150.2',
                                         dhcp_range_end='192.168.150.254',
-                                        mac_address_0='aa:bb:cc:dd:ee:ff',
-                                        ip_address_0='192.168.150.15')
+                                        hosts=hosts)
 
     try:
         # create a transient virtual network
